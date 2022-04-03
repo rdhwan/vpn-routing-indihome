05dd591d1981c4ba6935ca3f6b46124972b2db11
Fathan Ridhwan <89518861+rdhwan@users.noreply.github.com>
2022-04-03 13:22:23 +0700

    update

--- checker/check_google.py
+++ checker/check_google.py
@@ -1,12 +1,12 @@
-import requests
+import requests as r
 
-host = 'https://redirector.googlevideo.com/report_mapping', 'https://redirector.gvt1.com/report_mapping', 'https://redirector-bigcache.googleapis.com/report_mapping'
+hosts = 'https://redirector.googlevideo.com/report_mapping', 'https://redirector.gvt1.com/report_mapping', 'https://redirector-bigcache.googleapis.com/report_mapping'
 
 def main():
-    for hosts in host:
-        r = requests.get(hosts)
-        info = r.text.split('\n')[0]
-        print(hosts, ' : ', info)
+    for host in hosts:
+        res = r.get(host)
+        info = res.text.split('\n')[0]
+        print(host, ' : ', info)
 
 if __name__ == '__main__':
       main()
